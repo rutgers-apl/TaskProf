ea6024cdacaff5c410071fca0116e36ed576751b
Adarsh Yoga <adarsh.yoga@cs.rutgers.edu>
2017-06-25 18:50:42 -0400

    updated build file with meaningful message when performance counters not available

--- build.sh
+++ build.sh
@@ -3,31 +3,29 @@
 perf list | grep "cpu-cycles OR cycles" &> /dev/null
 if [ $? != 0 ]; then
     echo "Hardware Performance Counters not enabled. TaskProf requires a machine with Hardware Performance Counters. Exiting build."
-    exit 1
+else
+    root_cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #
+    echo $root_cwd
+
+    echo "***** Building TaskProf *****"
+    export LD_LIBRARY_PATH=""
+    cd $root_cwd
+    cd ptprof_lib/collection_phase
+    make clean
+    make
+    source tpvars.sh
+
+    cd $root_cwd
+    cd ptprof_lib/analysis_phase
+    make clean
+    make
+    source tpvars.sh
+
+    echo "***** Building TBB *****"
+    cd $root_cwd
+    cd tprof-tbb-lib
+    make clean
+    make
+    source obj/tbbvars.sh
+    cd $root_cwd
 fi
-
-
-root_cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #
-echo $root_cwd
-
-echo "***** Building TaskProf *****"
-export LD_LIBRARY_PATH=""
-cd $root_cwd
-cd ptprof_lib/collection_phase
-make clean
-make
-source tpvars.sh
-
-cd $root_cwd
-cd ptprof_lib/analysis_phase
-make clean
-make
-source tpvars.sh
-
-echo "***** Building TBB *****"
-cd $root_cwd
-cd tprof-tbb-lib
-make clean
-make
-source obj/tbbvars.sh
-cd $root_cwd
